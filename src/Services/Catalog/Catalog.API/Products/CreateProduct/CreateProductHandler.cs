2b76aa747d471281c3f9e85b90db63aac0e1c217
Ravi Kathalkar <ravikathalkar@gmail.com>
2025-02-09 13:35:19 +0530

    Catalog POST Endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,27 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+public record CreateProductCommand(string Name, string Description, decimal Price, string ImageFile, List<string> Category):ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+	public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+	{
+		//create a product entity from command object.
+		var product = new Product
+		{
+			Name = command.Name,
+			Description = command.Description,
+			Price = command.Price,
+			ImageFile = command.ImageFile,
+			Category = command.Category
+		};
+
+		//save the product entity to the database.
+
+		//return the product id.
+		return new CreateProductResult(Guid.NewGuid());
+	}
+}
